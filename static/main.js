37a72288130b90df30ceb23cedde46b95f2edaf4
Jonathan Cai <caimjonathan@gmail.com>
2018-07-31 10:24:07 -0700

    update static files

--- static/main.js
+++ static/main.js
@@ -15,65 +15,6 @@ $(document).ready(function () {
 
     $("#gitsave").click(function () { // save config
         var token = $("#gittoken").val()
-        if (token == "") {
-            window.alert("Enter non-empty token")
-            return
-        }
-        var json = JSON.stringify({
-            Token: token
-        })
-        $.ajax({
-            method: "POST",
-            url: APIurl + "token",
-            data: json,
-            dataType: "json"
-        })
-    });
-
-    $("#gitrepos").click(function () { // list repos
-        // var token = $("#gittoken").val()
-        $.ajax({
-            method: "GET",
-            url: APIurl + "repos",
-            success: function (result, status, jgXHR) {
-                $("#gitrepositories").text(result)
-            }
-        })
-    });
-
-    $("#gitcreate").click(function () { // create hook
-        var token = $("#gittoken").val()
-        var username = $("#gitusername").val()
-        var repo = $("#gitrepo").val()
-        var hookname = $("#githookname").val()
-        var pushconfig = document.getElementById("gitpush").checked
-        var pullconfig = document.getElementById("gitpullrequest").checked
-        if (!pushconfig &&  !pullconfig) {
-            window.alert("Must check atleast one event type when creating a hook")
-            return
-        }
-        if (token === "" || username === "" || repo === "" || hookname === "") {
-            window.alert("Token, username, repository name, and hook name fields need to be non-empty")
-            return
-        }
-        var json = JSON.stringify({
-            Token: token,
-            Username: username,
-            Repo: repo,
-            PushConfig: pushconfig,
-            PullConfig: pullconfig,
-            HookName: hookname,
-        })
-        $.ajax({
-            method: "POST",
-            url: APIurl + "create",
-            dataType: "json",
-            data: json,
-        })
-    });
-    
-    $("#gitlist").click(function () { // list hooks
-        var token = $("#gittoken").val()
         var username = $("#gitusername").val()
         var repo = $("#gitrepo").val()
         if (token === "" || username === "" || repo === "") {
@@ -86,37 +27,71 @@ $(document).ready(function () {
         })
         $.ajax({
             method: "POST",
-            url: APIurl + "list",
-            dataType: "json",
+            url: APIurl + "config",
             data: json,
+            dataType: "json"
+        })
+    });
+
+    $("#gitrepos").click(function () { // list repos
+        $.ajax({
+            method: "GET",
+            url: APIurl + "repos",
             success: function (result, status, jgXHR) {
-                // window.alert("Hello")
-                $("#githooks").text("result")
+                $("#gitrepositories").text(result)
             }
         })
     });
 
-    // function ListHook() {
-    //     return $.ajax({
-    //         method: "POST",
-    //         url: APIurl + "list",
-    //         dataType: "json",
-    //         data: json,
-    //     })
-
-    // }
+    $("#gitcreate").click(function () { // create hook
+        var hookurl = $("#githookurl").val()
+        var pushconfig = document.getElementById("gitpush").checked
+        var pullconfig = document.getElementById("gitpullrequest").checked
+        if (!pushconfig &&  !pullconfig) {
+            window.alert("Must check atleast one event type when creating a hook")
+            return
+        }
+        if (hookurl === "") {
+            window.alert("Hook name field need to be non-empty")
+            return
+        }
+        var json = JSON.stringify({
+            PushConfig: pushconfig,
+            PullConfig: pullconfig,
+            HookURL: hookurl,
+        })
+        $.ajax({
+            method: "POST",
+            url: APIurl + "create",
+            dataType: "json",
+            data: json,
+        })
+    });
+    
+    $("#gitlist").click(function () { // list hooks
+        $.ajax({
+            method: "GET",
+            url: APIurl + "list",
+            dataType: "json",
+            success: function (result, status, jgXHR) {
+                $("#githooks").empty() // Wipe out all previously loaded hooks
+                var p = result
+                count = 0
+                for (var key in p) {
+                    if (p.hasOwnProperty(key)) {
+                        count++
+                        var hook = "<div class='hook'><p>  Hook #  " +count.toString()+ "-  URL:  "+ key + ", Events:  " +p[key] +"</p></div>";
+                        $("#githooks").append(hook)
+                        console.log(key + " -> " + p[key]);
+                    }                
+                }
+            }
+        })
+    });
 
     $("#gitedit").click(function () { // edit hook
         window.alert("Nothing configured yet")
         return
-        // var token = $("#gittoken").val()
-        // $.ajax({
-        //     method: "GET",
-        //     url: APIurl + "repos",
-        //     success: function (result, status, jgXHR) {
-        //         $("#gitrepositories").text(result)
-        //     }
-        // })
     });
 
 });
